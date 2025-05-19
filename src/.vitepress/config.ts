0fd323de0a952d1f3cdb113587de898810c58112
Eduard Smet <contact@celarye.dev>
2025-05-19 19:24:54 +0200

    chore: Update dependencies and fix formatting issues

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -7,38 +7,38 @@ const hostname = "https://inkdex.github.io";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Inkdex",
-  description: "Community-Made Extensions for Paperback",
+    title: "Inkdex",
+    description: "Community-Made Extensions for Paperback",
 
-  head: headConfig,
+    head: headConfig,
 
-  cleanUrls: true,
+    cleanUrls: true,
 
-  lastUpdated: true,
+    lastUpdated: true,
 
-  sitemap: {
-    hostname,
-  },
-
-  themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
-    logo: "/logo-rounded.svg",
-
-    search: {
-      provider: "local",
+    sitemap: {
+        hostname,
     },
 
-    nav: navConfig,
+    themeConfig: {
+        // https://vitepress.dev/reference/default-theme-config
+        logo: "/logo-rounded.svg",
 
-    sidebar: sidebarConfig,
+        search: {
+            provider: "local",
+        },
 
-    editLink: {
-      pattern:
-        "https://github.com/inkdex/inkdex.github.io/tree/master/src/:path",
+        nav: navConfig,
+
+        sidebar: sidebarConfig,
+
+        editLink: {
+            pattern:
+                "https://github.com/inkdex/inkdex.github.io/tree/master/src/:path",
+        },
+
+        footer: {
+            copyright: "Copyright © 2025 Inkdex. GPLv3 Licensed.",
+        },
     },
-
-    footer: {
-      copyright: "Copyright © 2025 Inkdex. GPLv3 Licensed.",
-    },
-  },
 });
