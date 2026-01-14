73465ae87d8ee949bd6b7ecf8034edc6149cc677
Eduard Smet <contact@celarye.dev>
2026-01-14 10:15:10 +0100

    Add missing LICENSE headers (#29)

--- src/.vitepress/theme/components/ExtensionList.vue
+++ src/.vitepress/theme/components/ExtensionList.vue
@@ -1,3 +1,6 @@
+<!-- SPDX-License-Identifier: GPL-3.0-or-later -->
+<!-- Copyright © 2026 Inkdex -->
+
 <script setup lang="ts">
 import { computed, nextTick, onMounted, ref, watch } from "vue";
 import ExtensionCard from "./ExtensionCard.vue";
